ff32cfaa459a7a0ca7101302e014f819ab3e0bb6
SuhasiniTempalli <suhasini.nov18@gmail.com>
2025-01-18 21:11:17 -0600

    new changes

--- src/main/java/com/example/brewapplication/services/BeerServiceImpl.java
+++ src/main/java/com/example/brewapplication/services/BeerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.brewapplication.services;
 
 import com.example.brewapplication.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,6 +15,22 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    /**
+     * @param beerDto
+     * @return
+     */
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId,BeerDto beerDto){
+        //todo impl - would add a real impl to update beergit
     }
 }
 
